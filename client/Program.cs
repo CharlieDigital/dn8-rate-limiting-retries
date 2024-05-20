f3ca19e1f303735c1655fa992663c303c8b24736
Charles Chen <chen.charles.c@gmail.com>
2024-05-20 15:19:34 -0400

    Initial commit

---
+++ client/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.Json;
+using Polly;
+using Polly.Retry;
+
+using var client = new HttpClient();
+
+// Construct the pipeline
+var pipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
+  .AddRetry(
+    new RetryStrategyOptions<HttpResponseMessage>
+    {
+      // The number of retry attempts
+      MaxRetryAttempts = 4,
+      // The predicate that determines whether the result should be handled by the pipeline
+      ShouldHandle = new PredicateBuilder<HttpResponseMessage>()
+        .HandleResult(r =>
+          r.StatusCode == System.Net.HttpStatusCode.TooManyRequests
+        )
+        .Handle<HttpRequestException>(),
+      // A delay generation strategy that reads the Retry-After header.
+      DelayGenerator = static args =>
+      {
+        if (
+          args.Outcome.Result is HttpResponseMessage responseMessage
+          && TryGetDelay(responseMessage, out TimeSpan delay)
+        )
+        {
+          Console.WriteLine($"⮑  Setting retry delay to: {delay}");
+
+          return new ValueTask<TimeSpan?>(delay);
+        }
+
+        // Returning null means the retry strategy will use its internal delay for this attempt.
+        return new ValueTask<TimeSpan?>((TimeSpan?)null);
+
+        static bool TryGetDelay(
+          HttpResponseMessage response,
+          out TimeSpan delay
+        )
+        {
+          if (response.Headers.TryGetValues("Retry-After", out var values))
+          {
+            delay = TimeSpan.FromSeconds(
+              int.Parse(values.FirstOrDefault() ?? "0")
+            );
+
+            return true;
+          }
+
+          delay = TimeSpan.FromSeconds(0);
+
+          return false;
+        }
+      }
+    }
+  )
+  .Build();
+
+var completed = 0;
+
+// Execute 10 tasks to observe the behavior of the retries.
+var tasks = Enumerable
+  .Range(0, 10)
+  .Select(async i =>
+  {
+    await pipeline.ExecuteAsync(
+      async (state, token) =>
+      {
+        Console.WriteLine($"Sending request: {state.i}");
+
+        var response = await state.client.GetAsync(
+          $"http://localhost:5020/{state.i}"
+        );
+
+        var header = JsonSerializer.Serialize(
+          response.Headers.ToDictionary(h => h.Key, h => h.Value)
+        );
+
+        var prefix = response.IsSuccessStatusCode ? "✅" : "⛔️";
+
+        if (response.IsSuccessStatusCode)
+        {
+          Interlocked.Increment(ref completed);
+        }
+
+        Console.WriteLine(
+          $"{prefix} Code: {response.StatusCode}, Headers: {header}"
+        );
+
+        return response;
+      },
+      (client, i, completed)
+    );
+  });
+
+await Task.WhenAll([.. tasks]);
+
+Console.WriteLine($"Completed: {completed}");
---
+++ service/Program.cs
@@ -0,0 +1,72 @@
+using System.Globalization;
+using System.Net;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure the rate limiting behavior here.
+builder.Services.AddRateLimiter(_ =>
+{
+  _.AddSlidingWindowLimiter(
+    policyName: "4_concurrent_sliding",
+    options =>
+    {
+      options.PermitLimit = 4;
+      options.Window = TimeSpan.FromSeconds(10);
+      options.SegmentsPerWindow = 5;
+      options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+      options.QueueLimit = 2;
+      options.AutoReplenishment = true;
+    }
+  );
+
+  // The default status code is 503; we want to send a 429 instead.
+  _.RejectionStatusCode = (int)HttpStatusCode.TooManyRequests;
+
+  // Send a retry after with a random time.
+  _.OnRejected = async (context, cancel) =>
+  {
+    var retryAfter = TimeSpan.FromSeconds(Random.Shared.Next(4, 10));
+
+    context.HttpContext.Response.Headers.Append(
+      HeaderNames.RetryAfter,
+      ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo)
+    );
+
+    Console.WriteLine(
+      $"Sending {HeaderNames.RetryAfter} with value {retryAfter.TotalSeconds}"
+    );
+
+    await ValueTask.CompletedTask;
+  };
+});
+
+var app = builder.Build();
+
+app.UseRateLimiter();
+
+var requests = 0;
+
+// An HTTP GET endpoint which simulates some long running work that's 2.5 to 6s.
+app.MapGet(
+    "/{job}",
+    async (int job, HttpContext context) =>
+    {
+      var workload = Random.Shared.Next(2_500, 6_000);
+
+      await Task.Delay(workload);
+
+      context.Response.Headers.Append("x-job", job.ToString());
+
+      Interlocked.Increment(ref requests);
+
+      Console.WriteLine(
+        $"Executed workload {requests} for job {job} with time: {workload}ms"
+      );
+    }
+  )
+  .RequireRateLimiting("4_concurrent_sliding"); // Attach our rate limiting policy
+
+app.Run();
